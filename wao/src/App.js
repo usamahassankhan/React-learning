69c204b6a4b7d93e2660ddb75e20b9f1d3fc4033
usamahassankhan <usamahassankhan870@gmail.com>
2020-09-26 04:58:54 +0500

    ist project by me

---
+++ wao/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Bismillah hir rehman e reheem  ths is uhk</h1>
+    </div>
+  );
+}
+
+export default App;
