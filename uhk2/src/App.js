b16018bb67df9c8bee3b149dc2c411a04d4819ac
usamahassankhan <usamahassankhan870@gmail.com>
2020-09-26 22:58:38 +0500

    2nd app

---
+++ uhk2/src/App.js
@@ -0,0 +1,28 @@
+import React, { createElement } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>this is me  practice</h1>
+//     </div>
+//   );
+// }
+function App(){
+  return (
+    createElement("div",{className:'App'},"noce this is googd")
+  );
+}
+// class App extends React.Component{
+//   render(){
+//     return( 
+//       <div className="App">
+//       <h1>this is main
+
+//       </h1>nice man</div>
+//     );
+//   }
+// }
+
+export default App;
