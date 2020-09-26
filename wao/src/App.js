0f9824610bceb36f2fee7211aebf8ea90d4105ca
usamahassankhan <usamahassankhan870@gmail.com>
2020-09-26 12:37:03 +0500

    its react bro

--- wao/src/App.js
+++ wao/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 // function App() {
 //   return (
 //     <div className="App">
@@ -11,25 +12,47 @@ import './App.css';
 // }
 
 
+class Header extends React.Component{
+  render(){
+    return(
+      <div className="header">
+      <h1>This is Header</h1>
+      </div>
+    )
+  }
+}
+class Footer extends React.Component{
+  render(){
+    return(
+      <div className="footer">
+      <h1>This is Footer</h1>
+      </div>
+    )
+  }
+}
+
 
 class App extends React.Component{
   render(){
-    let wao=[{test:1},{test:2},{test:4},{test:5},{test:6}];
-    let name= "uhk";
+    // let wao=[{test:1},{test:2},{test:4},{test:5},{test:6}];
+    // let name= "uhk";
   return(
-    <div >
+    <div  className="main">
+      <Header/>
      {/* <h1>Bismillah hir rehman e reheem  ths is uhk</h1>
 <h1>{`this is  me ${name} abd ${name}`}</h1>
  <h1>{wao}</h1> */}
  
-   <ul>
+   {/* <ul>
      {wao.map((item,index)=>{
       return <li key={index}>{`this is num ${index} of ${item.test}`}</li> 
      })}
-   </ul>
+   </ul> */}
+   <h1>Main Component</h1>
  
  
-    </div>
+
+ <Footer/>    </div>
   )
 }}
 
