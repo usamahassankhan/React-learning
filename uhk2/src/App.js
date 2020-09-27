0d0c60cfdefc18d63478a482c07e7b5efa790c41
usamahassankhan <usamahassankhan870@gmail.com>
2020-09-27 21:44:40 +0500

    button

--- uhk2/src/App.js
+++ uhk2/src/App.js
@@ -11,24 +11,35 @@ import person from './person/person';
 //     </div>
 //   );
 // }
-function App(){
-  return (
-   
-// {/* <Person name="uhk"/> */}
-
-<Person name="uhk">nice work</Person>
-
-  );
-}
-// class App extends React.Component{
-//   render(){
-//     return( 
-//       <div className="App">
-//       <h1>this is main
-
-//       </h1>nice man</div>
-//     );
+// function App(){
+//   state={
+//     person:[{
+//       name:"usama",
+//       age:"21"
+//     }]
 //   }
+//   return (
+   
+// // {/* <Person name="uhk"/> */}
+
+// <Person name={this.state.person[0].nam}e>nice work</Person>
+
+//   );
 // }
+class App extends React.Component{
+  state={
+        person:[{
+          name:"usama",
+          age:"21"
+        }]
+      }
+  render(){
+    return( 
+      
+      <Person name={this.state.person[0].name}>nice work</Person>
+      
+    );
+  }
+}
 
 export default App;
